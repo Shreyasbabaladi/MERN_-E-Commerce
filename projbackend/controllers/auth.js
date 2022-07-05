52c5396348e4a39327952c0d22b7e1af65a7b16c
shreyasbabaladi <shreyasbabaladi402@gmail.com>
2022-07-05 21:14:40 +0530

    completed signin controler

--- projbackend/controllers/auth.js
+++ projbackend/controllers/auth.js
@@ -1,31 +1,70 @@
 const User = require("../models/user");
-const { check, validationResult } = require('express-validator');
+const { check, validationResult } = require("express-validator");
+var jwt = require("jsonwebtoken");
+var expressJwt = require("express-jwt");
 
+// Controller for signup
 exports.signup = (req, res) => {
-    const errors = validationResult(req);
+  const errors = validationResult(req);
 
-    if(!errors.isEmpty()){
-        return res.status(422).json({
-            error: errors.array()[0].msg
-        })
-    }
+  if (!errors.isEmpty()) {
+    return res.status(422).json({
+      error: errors.array()[0].msg,
+    });
+  }
   const user = new User(req.body);
   user.save((err, user) => {
     if (err) {
       return res.status(400).json({
-        err: "NOT able to save user in DB"
+        err: "NOT able to save user in DB",
       });
     }
     res.json({
       name: user.name,
       email: user.email,
-      id: user._id
+      id: user._id,
     });
   });
 };
 
+// Controller for signin
+exports.signin = (req, res) => {
+  const errors = validationResult(req);
+  const { email, password } = req.body;
+
+  if (!errors.isEmpty()) {
+    return res.status(422).json({
+      error: errors.array()[0].msg,
+    });
+  }
+
+  User.findOne({ email }, (err, user) => {
+    if (err || !user){
+      return res.status(400).json({
+        error: "USER email does not exists",
+      });
+    }
+
+    if (!user.autheticate(password)) {
+      return res.status(401).json({
+        error: "Email and password do not match",
+      });
+    }
+
+    //create token
+    const token = jwt.sign({ _id: user._id }, process.env.SECRET);
+    //put token in cookie
+    res.cookie("token", token, { expire: new Date() + 30 });
+
+    //send data to  front end
+    const { _id, name, email, role } = user;
+    return res.json({ token, user: { _id, name, email, role } });
+  });
+};
+
+// Controller for signout
 exports.signout = (req, res) => {
   res.json({
-    message: "User signout"
+    message: "User signout",
   });
 };
--- projbackend/routes/auth.js
+++ projbackend/routes/auth.js
@@ -1,22 +1,27 @@
 const express = require("express");
 const router = express.Router();
-const { signout, signup } = require("../controllers/auth");
+const { signin, signup } = require("../controllers/auth");
 const { check } = require("express-validator");
 
+//Router for signup
 router.post(
   "/signup",
   [
-    check("name")
-      .isLength({ min: 3 })
-      .withMessage("name must be at least 3 chars long"),
-    check("email").isEmail().withMessage("Email is requrired"),
-    check("password")
-      .isLength({ min: 3 })
-      .withMessage("password must be at least 3 chars long"),
+    check("name").isLength({ min: 3 }).withMessage("name must be at least 3 chars long"),
+    check("email").isEmail().withMessage("Email is required"),
+    check("password").isLength({ min: 3 }).withMessage("password must be at least 3 chars long"),
   ],
   signup
 );
 
-router.get("/signout", signout);
+//Router for signin
+router.post(
+  "/signin",
+  [
+    check("email").isEmail().withMessage("Email is required"),
+    check("password").isLength({ min: 1 }).withMessage("password field is required"),
+  ],
+  signin
+);
 
 module.exports = router;
