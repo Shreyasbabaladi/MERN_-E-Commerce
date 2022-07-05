099b1496ce6ba19d2ffb0aa33c7c9aec0ee58ad6
shreyasbabaladi <shreyasbabaladi402@gmail.com>
2022-07-05 19:34:07 +0530

    storing the user the data in data base

--- projbackend/routes/auth.js
+++ projbackend/routes/auth.js
@@ -1,8 +1,21 @@
 const express = require("express");
 const router = express.Router();
 const { signout, signup } = require("../controllers/auth");
+const { check } = require("express-validator");
 
-router.post("/signup", signup);
+router.post(
+  "/signup",
+  [
+    check("name")
+      .isLength({ min: 3 })
+      .withMessage("name must be at least 3 chars long"),
+    check("email").isEmail().withMessage("Email is requrired"),
+    check("password")
+      .isLength({ min: 3 })
+      .withMessage("password must be at least 3 chars long"),
+  ],
+  signup
+);
 
 router.get("/signout", signout);
 
