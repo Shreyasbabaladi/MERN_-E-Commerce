b330c1bac4949ae93673d2d10176b846b2ed3a25
shreyasbabaladi <shreyasbabaladi402@gmail.com>
2022-07-17 22:05:49 +0530

    stated with forntend

---
+++ projfrontend/src/Routes.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { BrowserRouter, Routes as Switch, Route } from "react-router-dom";
+import Home from "./core/Home";
+
+export default function Routes() {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact element={<Home/>} />
+      </Switch>
+    </BrowserRouter>
+  );
+}
---
+++ projfrontend/src/core/Home.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import '../styles.css'
+
+export default function Home() {
+  return (
+    <div><h1>Shreyas</h1></div>
+  )
+}
